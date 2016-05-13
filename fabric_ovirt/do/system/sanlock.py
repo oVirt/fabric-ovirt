8c193b0b308e175b648cc9b6750577500679c207
David Caro <dcaroest@redhat.com>
2016-05-13 10:47:04 +0200

    Fixes to system.sanlock
    * Now aborting on sanlock freeing failures
    * Refactored a bit
    
    Change-Id: I4640f9c0ca5509c4eb2dcd8af6e78a6fe70c157a
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- fabric_ovirt/do/system/sanlock.py
+++ fabric_ovirt/do/system/sanlock.py
@@ -12,6 +12,7 @@ from fabric.api import (
     run,
 )
 from fabric_ovirt.lib.utils import (
+    abort,
     info,
 )
 
@@ -19,25 +20,36 @@ from fabric_ovirt.lib.utils import (
 @task
 def clear():
     with settings(
-            hide('status', 'running', 'stdout', 'stderr', 'warnings'),
-            warn_only=True,):
+        hide('status', 'running', 'stdout', 'stderr', 'warnings'),
+        warn_only=True,
+    ):
         out = run("rpm -q sanlock")
     if not out.succeeded:
         info("Sanlock was not installed.")
         return
+
     out = run("sanlock client status")
     if out.failed:
         fail("Failed to check sanlock status")
-    locks = []
-    for line in out.splitlines():
-        if line.startswith('s '):
-            locks.append(line.split(' ')[-1])
+
+    locks = [
+        line.rsplit(' ', 1)[-1] for line in out.splitlines()
+        if line.startswith('s ')
+    ]
     info("Got %d locks" % len(locks))
+    failed_locks = []
     for lock in locks:
         info("  Freeing lock %s" % lock)
         with settings(
-                hide('running', 'stdout', 'stderr', 'warnings'),
-                warn_only=True,
-                disable_known_hosts=True):
-            run("sanlock rem_lockspace -s '%s'" % lock)
+            hide('running', 'stdout', 'stderr', 'warnings'),
+            warn_only=True,
+            disable_known_hosts=True
+        ):
+            res = run("sanlock rem_lockspace -s '%s'" % lock)
+            if not res.succeeded:
+                failed_locks.append(lock)
+
+    if failed_locks:
+        abort("Failed to freed locks: " + ','.join(failed_locks))
+
     info("Done")
